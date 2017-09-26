14d2f1b2d4da9d18f6439f4f6346c69f690efda2
ikennanwagwu <ikennanwagwu@wirelessprv-10-192-19-99.near.illinois.edu>
2017-09-26 18:26:06 -0500

    corrected changes for lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,15 +1,14 @@
 /*
- * The following code needs a lot of TLC. So give it some!
- *
- * 1. Fix all checkstyle errors
- * 2. Determine what it does (it's going to be tough before you finish #1)
- * 3. Improve the name of the methods and variables
- * 4. Add comments and Javadoc comments where needed
- * 5. Remove unnecessary comments as appropriate
+ * The following code needs a lot of TLC. So give it some! 1. Fix all checkstyle errors 2. Determine
+ * what it does (it's going to be tough before you finish #1) 3. Improve the name of the methods and
+ * variables 4. Add comments and Javadoc comments where needed 5. Remove unnecessary comments as
+ * appropriate
  */
-
+/**
+ *
+ */
 public class ReclamationProject {
-   public static String doit(String a, String b) {
+    public static String doit(final String a, final String b) {
 
         if (a.length() > b.length()) {
             String c = a;
@@ -24,11 +23,13 @@ public class ReclamationProject {
         for (int i = 0; i < a.length(); i++) {
             for (int j = a.length() - i; j > 0; j--) {
                 for (int k = 0; k < b.length() - j; k++) {
-                   r = (a.regionMatches(i, b, k, j) && j > r.length()); ? a.substring(i, i + j) != null : r; // Do it!
-                        }
-                }
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
 
+                }
+                return r;
+            }
         }
-     return r;
-   }
+    }
 }
